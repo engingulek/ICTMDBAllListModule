0aa80d6d4fe7f88d9b53a5c2b59bacac408281e8
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-11-12 20:15:15 +0300

    created view design

--- ICTMDBAllListModule/ICTMDBAllListModule.swift
+++ ICTMDBAllListModule/ICTMDBAllListModule.swift
@@ -7,3 +7,42 @@
 
 import Foundation
 
+import UIKit
+ import ICTMDBModularProtocols
+ import ICTMDBNetworkManagerKit
+
+
+public class ICTMDBAllListModule : @preconcurrency AllListModuleProtocol {
+ 
+    public init() { }
+    
+    @MainActor public func createAllListModule(type: AllListType) -> UIViewController {
+        let viewController = AllListViewController()
+        let router = AllListRouter()
+        let interactor = AllListInteractor()
+        
+        let presenter : any ViewToPresenterAllListProtocol & InteractorToPresenterAllListProtocol
+        = AllListPresenter(view: viewController, interactor: interactor, router: router)
+        viewController.presenter = presenter
+        interactor.presenter = presenter
+        let listType:ListType = type == .popular ? .popular : .airingToday
+        presenter.getAllList(at: listType)
+        return viewController
+    }
+    
+    
+    
+    static func mockCreateAllListModule() -> UIViewController {
+        let viewController = AllListViewController()
+        let router = AllListRouter()
+        let interactor = AllListInteractor()
+        
+        let presenter : any ViewToPresenterAllListProtocol & InteractorToPresenterAllListProtocol
+        = AllListPresenter(view: viewController, interactor: interactor, router: router)
+        viewController.presenter = presenter
+        interactor.presenter = presenter
+     
+        presenter.getAllList(at: .popular)
+        return viewController
+    }
+}
